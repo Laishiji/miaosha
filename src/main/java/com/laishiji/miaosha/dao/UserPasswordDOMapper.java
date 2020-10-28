f42fe0b506b209ccdb1a3dd9bea96e5d7ca69728
lsj <laishiji86@gmail.com>
2020-10-28 22:28:52 +0800

    remove Example

--- src/main/java/com/laishiji/miaosha/dao/UserPasswordDOMapper.java
+++ src/main/java/com/laishiji/miaosha/dao/UserPasswordDOMapper.java
@@ -1,28 +1,15 @@
 package com.laishiji.miaosha.dao;
 
 import com.laishiji.miaosha.dataobject.UserPasswordDO;
-import com.laishiji.miaosha.dataobject.UserPasswordDOExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface UserPasswordDOMapper {
-    long countByExample(UserPasswordDOExample example);
-
-    int deleteByExample(UserPasswordDOExample example);
-
     int deleteByPrimaryKey(Integer id);
 
     int insert(UserPasswordDO record);
 
     int insertSelective(UserPasswordDO record);
 
-    List<UserPasswordDO> selectByExample(UserPasswordDOExample example);
-
     UserPasswordDO selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") UserPasswordDO record, @Param("example") UserPasswordDOExample example);
-
-    int updateByExample(@Param("record") UserPasswordDO record, @Param("example") UserPasswordDOExample example);
 
     int updateByPrimaryKeySelective(UserPasswordDO record);
 
